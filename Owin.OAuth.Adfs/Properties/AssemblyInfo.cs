c954ef2ec8e26ff7f662e19672763a6bcf0f7c69
CrescentFresh <i.jaypot@gmail.com>
2016-02-17 17:06:55 -0500

    rename project

--- Owin.OAuth.Adfs/Properties/AssemblyInfo.cs
+++ Owin.OAuth.Adfs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Owin.Authentication.Adfs")]
+[assembly: AssemblyTitle("Owin.OAuth.Adfs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Owin.Authentication.Adfs")]
+[assembly: AssemblyProduct("Owin.OAuth.Adfs")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
